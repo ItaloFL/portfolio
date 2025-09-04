3334177a3ffdc602e122d7d35e2b73cd1d8d55d4
ItaloFL <grd788@gmail.com>
2025-09-04 15:18:32 -0500

    feat :sparkles: scroll to top function

--- src/layout/defaultLayout.tsx
+++ src/layout/defaultLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
+import { ScrollToTop } from "@/lib/scrollToTop";
 
 export function DefaultLayout() {
   return (
     <div className="">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
